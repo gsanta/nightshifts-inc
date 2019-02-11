00eda7ccf6e25452dd7f286b368df39df34467d9
gsanta <gsanta@chemaxon.com>
2019-02-11 17:56:00 +0100

    Unit-test for GetWorldAction 2.

--- src/client/state/game/actions/GetWorldAction.test.ts
+++ src/client/state/game/actions/GetWorldAction.test.ts
@@ -1,16 +1,42 @@
 import { GetWorldActions } from './GetWorldActions';
 import { expect } from 'chai';
-import { take } from 'redux-saga/effects';
+import { take, put, call } from 'redux-saga/effects';
 import { ActionType } from '../../../stores/ActionType';
+import { GameRequests } from '../GameRequests';
+import * as sinon from 'sinon';
 
 
 describe('GetWorldAction', () => {
     describe('watchOnce', () => {
-        it.only ('calls the right actions', () => {
+        it ('calls the right actions', () => {
             const watchOnce = GetWorldActions.watchOnce();
             const val = watchOnce.next().value;
 
             expect(val).to.eql(take(ActionType.GET_USER_SUCCESS));
+        });
+    });
+
+    describe('fetch', () => {
+        it.only ('calls the webservice and and gets the \'world\'', () => {
+            const fetch = GetWorldActions.fetch();
+            const worldMock = sinon.spy();
+            const userMock = sinon.spy();
+
+            const getWorldByUserId = sinon.stub();
+            getWorldByUserId.withArgs(userMock).resolves(worldMock);
+            const gameRequestMock: Partial<GameRequests> = {
+                getWorldByUserId
+            };
+
+
+            fetch.next();
+            fetch.next(gameRequestMock);
+
+            const callGetWorld = fetch.next(userMock);
+            expect(callGetWorld).to.eql(call([gameRequestMock, gameRequestMock.getWorldByUserId], <any> userMock));
+
+            const putGetWorldSuccess = fetch.next(worldMock);
+            expect(putGetWorldSuccess).to.eql(put({type: ActionType.GET_WORLD_SUCCESS, worldMock}));
         });
     });
 });
