bb87e45699cabfa13a7e7e5041988972c46acd3e
gsanta <gsanta@chemaxon.com>
2019-04-26 08:20:24 +0200

    Simplifying mesh creation 2.

--- src/game/model/core/factories/MeshFactory.ts
+++ src/game/model/core/factories/MeshFactory.ts
@@ -1,7 +1,6 @@
 import { WorldItem } from '../../../world_items/WorldItem';
 import { World } from '../../World';
-import { Polygon, GwmWorldItem } from 'game-worldmap-generator';
-import { Mesh } from 'babylonjs';
+import { GwmWorldItem } from 'game-worldmap-generator';
 import { ModelFactory } from '../../../io/gwm_world_io/import/factories/ModelFactory';
 import { WorldItemFactory } from './WorldItemFactory';
 
@@ -9,33 +8,33 @@ export interface GenericItemImporter<T> {
     createItem(itemInfo: T, world: World): WorldItem;
 }
 
-export class MeshFactory<T> {
-    private factories: {[key: string]: GenericItemImporter<T>};
+export class MeshFactory {
+    private factories: {[key: string]: GenericItemImporter<GwmWorldItem>};
     private worldItemFactoryMap: Map<string, WorldItemFactory>;
     private bedFactory: ModelFactory;
 
-    constructor(worldItemFactoryMap: Map<string, WorldItemFactory>, factories: {[key: string]: GenericItemImporter<T>}) {
+    constructor(worldItemFactoryMap: Map<string, WorldItemFactory>, factories: {[key: string]: GenericItemImporter<GwmWorldItem>}) {
         this.factories = factories;
         this.worldItemFactoryMap = worldItemFactoryMap;
     }
 
-    public createWall(itemInfo: T, world: World): WorldItem {
+    public createWall(itemInfo: GwmWorldItem, world: World): WorldItem {
         return this.factories.wall.createItem(itemInfo, world);
     }
 
-    public createPlayer(itemInfo: T, world: World): WorldItem {
+    public createPlayer(itemInfo: GwmWorldItem, world: World): WorldItem {
         return this.factories.player.createItem(itemInfo, world);
     }
 
-    public createWindow(itemInfo: T, world: World): WorldItem {
+    public createWindow(itemInfo: GwmWorldItem, world: World): WorldItem {
         return  this.factories.window.createItem(itemInfo, world);
     }
 
-    public createDoor(itemInfo: T, world: World): WorldItem {
+    public createDoor(itemInfo: GwmWorldItem, world: World): WorldItem {
         return this.factories.door.createItem(itemInfo, world);
     }
 
-    public createFloor(itemInfo: T, world: World): WorldItem {
+    public createFloor(itemInfo: GwmWorldItem, world: World): WorldItem {
         return this.factories.floor.createItem(itemInfo, world);
     }
 
@@ -43,23 +42,23 @@ export class MeshFactory<T> {
         return this.worldItemFactoryMap.get('bed').createItem(itemInfo, world);
     }
 
-    public createTable(itemInfo: T, world: World): WorldItem {
+    public createTable(itemInfo: GwmWorldItem, world: World): WorldItem {
         return this.factories.table.createItem(itemInfo, world);
     }
 
-    public createBathtub(itemInfo: T, world: World): WorldItem {
+    public createBathtub(itemInfo: GwmWorldItem, world: World): WorldItem {
         return this.factories.bathtub.createItem(itemInfo, world);
     }
 
-    public createWashbasin(itemInfo: T, world: World): WorldItem {
+    public createWashbasin(itemInfo: GwmWorldItem, world: World): WorldItem {
         return this.factories.washbasin.createItem(itemInfo, world);
     }
 
-    public createCupboard(itemInfo: T, world: World): WorldItem {
+    public createCupboard(itemInfo: GwmWorldItem, world: World): WorldItem {
         return this.factories.cupboard.createItem(itemInfo, world);
     }
 
-    public createRoom(itemInfo: T, world: World): WorldItem {
+    public createRoom(itemInfo: GwmWorldItem, world: World): WorldItem {
         return this.factories.room.createItem(itemInfo, world);
     }
 }
